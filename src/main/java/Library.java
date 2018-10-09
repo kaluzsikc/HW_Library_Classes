f5a42bb92ae5975d4ac3ec373fd8a283bd5cbfe5
kaluzsikc <kaluzsikc@gmail.com>
2018-10-09 22:15:46 +0100

    MVP completed

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collectionOfBooks;
+    private int capacity;
+
+    public Library(int capacity) {
+        this.collectionOfBooks = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public int countBooks() {
+        return collectionOfBooks.size();
+    }
+
+    public void addBook(Book book) {
+        if( capacity > collectionOfBooks.size()) {
+            collectionOfBooks.add(book);
+        }
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
 
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,5 +1,60 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
 
+    Library library;
+    Book book1;
+    Book book2;
+    Book book3;
+    Book book4;
+    Book book5;
+    Book book6;
+    Book capacity;
+
+
+    @Before
+    public void before() {
+        library = new Library(5);
+        book1 = new Book("Learn Java", "CodeClan", "SiFi");
+        book2 = new Book("Lethal White: Cormoran", "Robert Galbraith", "Mystery");
+        book3 = new Book("Set In Darkness", "Ian Rankin", "Crime");
+        book4 = new Book("Harry Potter 1", "J K Rowling", "Fantasy");
+        book5 = new Book("Harry Potter 2", "J K Rowling", "Fantasy");
+        book6 = new Book("Harry Potter 3", "J K Rowling", "Fantasy");
+    }
+
+    @Test
+    public void getLibraryStock() {
+        assertEquals(0, library.countBooks());
+    }
+
+    @Test
+    public void canAddBookToLibrary() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        assertEquals(4, library.countBooks());
+    }
+
+    @Test
+    public void canNotAddFullCapacity() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        library.addBook(book6);
+        assertEquals(5, library.countBooks());
+    }
+
+    @Test
+    public void getLibraryCapacity() {
+        assertEquals(5, library.getCapacity());
+    }
 
 }
 
